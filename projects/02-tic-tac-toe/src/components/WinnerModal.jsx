caaca86561f8f7944f35f482709768b2b10b35aa
Renzo Osorio <renzo.osorio1909@gmail.com>
2023-04-12 21:39:26 -0500

    agregando follow mouse y linter

--- projects/02-tic-tac-toe/src/components/WinnerModal.jsx
+++ projects/02-tic-tac-toe/src/components/WinnerModal.jsx
@@ -1,21 +1,21 @@
-import { Square } from "./Square";
+import { Square } from './Square'
 
-export function WinnerModal({ winner, resetGame }) {
-  if (winner === null) return null;
+export function WinnerModal ({ winner, resetGame }) {
+  if (winner === null) return null
 
-  const winnerText = winner === false ? "Empate" : "Ganó";
+  const winnerText = winner === false ? 'Empate' : 'Ganó'
 
   return (
-    <section className="winner">
-      <div className="text">
+    <section className='winner'>
+      <div className='text'>
         <h2>{winnerText}</h2>
 
-        <header className="win">{winner && <Square>{winner}</Square>}</header>
+        <header className='win'>{winner && <Square>{winner}</Square>}</header>
 
         <footer>
           <button onClick={resetGame}>Empezar de nuevo</button>
         </footer>
       </div>
     </section>
-  );
+  )
 }
